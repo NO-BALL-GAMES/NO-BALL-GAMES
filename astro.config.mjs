784fa74e1f567cb526424d6b3effee1314a36c26
Charlie Buster <charliebusteruk@gmail.com>
2026-06-07 15:50:05 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://NO-BALL-GAMES.github.io',
-  // If deploying as a project site instead of organization site, uncomment and update:
-  // base: '/NO-BALL-GAMES',
-  
+  site: 'https://no-ball-games.github.io',
+  base: '/NO-BALL-GAMES',
+
   integrations: [
     // Add additional integrations here as needed
   ],
