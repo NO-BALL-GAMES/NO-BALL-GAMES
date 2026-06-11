8b6ad7ddb3b2910e5ab4d57a5ac6c81643055364
HODLKONG64 <hodlkong64@gmail.com>
2026-06-11 21:00:41 +0100

    Remove broken sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,7 @@ export default defineConfig({
   site: 'https://no-ball-games.github.io',
   base: '/NO-BALL-GAMES/',
 
-  integrations: [
-    // Add additional integrations here as needed
-  ],
+  integrations: [],
 
   vite: {
     ssr: {
